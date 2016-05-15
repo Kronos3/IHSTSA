a0e163806b2b6b43da305266ae304d932f3ba1b9
tristanengst <tristanengst@gmail.com>
2016-05-15 00:49:23 -0400

    Update scroll.js

--- css/scroll.js
+++ css/scroll.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
             $(document).scroll(function() { 
                 scroll_pos = $(this).scrollTop();
                 if(scroll_pos > -1) {
-                    $("#back").css('background-color', '#585858');
+                    $("#back").css('background-color', 'blue');
                 }
                 else {
                     $("#back").css('background-color', 'rgba(255,255,255,0)');
