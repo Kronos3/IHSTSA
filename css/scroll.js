d61064496ebc171d1d3472db1f007093d6c107f4
tristanengst <tristanengst@gmail.com>
2016-05-15 00:53:33 -0400

    Update scroll.js

--- css/scroll.js
+++ css/scroll.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
             var scroll_pos = 0;
             $(document).scroll(function() { 
                 scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 0) {
+                if(scroll_pos > -1) {
                     $("#back").css('background-color', '#585858');
                 }
                 else {
-                    $("#back").css('background-color', '#585858');
+                    $("#back").css('background-color', 'rgba(255,255,255,0)');
                 }
             });
         });
