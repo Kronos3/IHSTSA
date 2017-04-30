d16b6593529b0f1e7133b04a3926a0c85c342a37
kronos3 <dovakhiin1359@gmail.com>
2017-04-29 22:11:24 -0400

    Did header for lander

--- js/main.js
+++ js/main.js
@@ -1,48 +1,117 @@
-$(window).scroll(function () {
-    try {
-    var y = $(window).scrollTop(),
-        readmore = $('#event-read-more').offset().top - $(window).height();
-        creator = $('#creator-box').offset().top - $(window).height();
-    if (y > creator) {
-        $( "#creator-box" ).children().css('opacity', '0');
-        $( "#creator-box" ).children().css('animation', 'creator-content-load 1s');
-        $( "#creator-box" ).children().css('animation-fill-mode', 'forwards');
-        $( "#creator-box" ).children().css('animation-delay', '1s');
-        $('#creator-box').css('animation', 'creators-load 1s');
-    }
-    }
-    catch (e) {};
-
-});
-
-var navOpened = 0;
-var navIn = "slideInRight";
-var navOut = "slideOutRight";
-
-function openNav () {
-    if (navOpened == 0) {
-        $("#nav").css("right", "0px");
-        $("#hamburger").css("color", "rgb(255, 255, 255)");
-        navOpened = 1;
-    }
-    else {
-        $("#nav").css("right", "-500px");
-        $("#hamburger").css("color", "rgb(80, 77, 71)");
-        navOpened = 0;
-    }
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
 }
 
-$( document ).ready(function() {
-    for (var i=0; i != $('.navmk2 > li').length; i++){
-        $('.navmk2 > li')[i].addEventListener('click', function () {
-            window.location.href = $(this).data ('href');
-        }, false);
+var star_list = ['circle', 'circle-s', 'circle-s-p', 'lines', 'square', 'square-s', 'square-s-p'];
+$(window).ready(function () {
+    
+    /* Create the stars */
+    for (var i = 0; i != star_list.length; i++) {
+        xhr = new XMLHttpRequest();
+        xhr.open("GET","img/assets/stars/{0}.svg".format (star_list[i]), false);
+        xhr.overrideMimeType("image/svg+xml");
+        xhr.send("");
+        var d = xhr.responseXML.documentElement;
+        $(".stars").get(0).appendChild(d);
+        $(".stars").get(0).appendChild(d.cloneNode(true));
+        $(".stars").get(0).appendChild(d.cloneNode(true));
+        $(".stars").get(0).appendChild(d.cloneNode(true));
+        $(".stars").get(0).appendChild(d.cloneNode(true));
     }
-    for (var i=0; i != $('.navmk3 > li').length; i++){
-        $('.navmk3 > li')[i].addEventListener('click', function () {
-            window.location.href = $(this).data ('href');
-        }, false);
+    
+    /* Set their positions randomly */
+    for (var i = 0; i != $('.stars > svg').length; i++) {
+        setup_pos_svg ($('.stars > svg').get(i), (Math.random() * (-0.04 - 0.04) + 0.04).toFixed(4),(Math.random() * (-0.120 - 0.12) + 0.12).toFixed(4));
+        $($('.stars > svg').get(i)).css ("top", (Math.random() * (80 - 0) + 0).toFixed(0) + "%");
+        $($('.stars > svg').get(i)).css ("left", (Math.random() * (140 - 0) + 0).toFixed(0) + "%");
     }
+    
+    /* Catch mouse movement */
+    //document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        var dot, eventDoc, doc, body, pageX, pageY;
+        event = event || window.event;
+        
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+        for (var i = 0; i != $('.stars > svg').length; i++) {
+            set_pos_svg ($('.stars > svg').get(i), event.pageX, event.pageY);
+        }
+    }
+    
+    /* Render other SVGs */
+    var __e = $('.render-svgs');
+    for (var i = 0; i != __e.length; i++) {
+        var current_parent = __e.get (i);
+        var _data = $(current_parent).data ('svgs').split(",");
+        for (var j = 0; j != _data.length; j++) {
+            xhr = new XMLHttpRequest();
+            xhr.open("GET","img/assets/{0}.svg".format (_data[j]), false);
+            xhr.overrideMimeType("image/svg+xml");
+            xhr.send("");
+            var d = xhr.responseXML.documentElement;
+            current_parent.appendChild(d);
+        }
+    }
+
 });
 
+function set_pos_svg (e, x, y) { // Use negative for inverse
+    $(e).css("transform", "matrix(1, 0, 0, 1, " + $(e).attr ('xfactor') * x + " , " + $(e).attr ('yfactor') * y + ")");
+}
 
+function set_pos (e, x, y) { // Use negative for inverse
+    $(e).css("transform", "matrix(1, 0, 0, 1, " + $(e).data ('xfactor') * x + " , " + $(e).data ('yfactor') * y + ")");
+}
+
+function setup_pos_svg (e, x_scale, y_scale) {
+    $(e).attr ('xfactor', x_scale);
+    $(e).attr ('yfactor', y_scale);
+}
+
+function setup_pos (e, x_scale, y_scale) {
+    $(e).data ('xfactor', x_scale);
+}
+
+function set_rot (e, x, y) { // Use negative for inverse
+    $(e).css("transform", "rotateY(" + x * $(e).data ('xfactor') + "deg)" + "rotateX(" + y * $(e).data ('yfactor') + "deg)");
+}
+
+var p_rot = 0;
+
+var lastScrollTop = 0;
+
+$(window).scroll (function (e) {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop){
+        p_rot += 1;
+    } else {
+        p_rot -= 1;
+    }
+    lastScrollTop = st;
+    $('#planetsvg').css ('transform', 'translateX(-50%) rotateZ({0}deg)'.format(p_rot));
+    if (st == 0) {
+        $('.nav').removeClass ('down');
+    }
+    else {
+        $('.nav').addClass ('down');
+    }
+});
